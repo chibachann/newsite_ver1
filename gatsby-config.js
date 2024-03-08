cd02212d26cdcf3360d90972aadeb373aaaeaf45
Chiba <ryotai2677@gmail.com>
2024-03-08 12:48:06 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `newsite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
